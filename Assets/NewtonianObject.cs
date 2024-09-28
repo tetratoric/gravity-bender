0c06e9ebf09d8bc9a06e5843ecfa3c14a55ec936
Tetratoric <114266944+tetratoric@users.noreply.github.com>
2024-09-27 23:36:31 -0500

    Gravity implementation

---
+++ Assets/NewtonianObject.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class NewtonianObject : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    float G;
+
+    public bool attractObjects = false;
+
+    public static List<NewtonianObject> newtonianObjects;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+
+        if (attractObjects) {
+            gameObject.tag = "Attractor";
+        }
+    }
+
+    void OnEnable() {
+        if (newtonianObjects == null) {
+            newtonianObjects = new List<NewtonianObject>();
+        }
+        newtonianObjects.Add(this);
+    }
+
+    void Start() {
+        G = GameManager.G;
+    }
+
+    void OnDisable() {
+        newtonianObjects.Remove(this);
+    }
+
+    void FixedUpdate() {
+        AttractToObjects();
+    }
+
+    void AttractToObjects() {
+        foreach (var attractor in newtonianObjects) {
+            
+            Vector3 displacement = attractor.transform.position - transform.position;
+            
+            if (attractor != this && displacement != Vector3.zero && attractor.tag == "Attractor") {
+                
+                Vector3 direction = displacement.normalized;
+                float distance = displacement.magnitude;
+
+                float forceMagnitude = G * (rb.mass * attractor.rb.mass) / distance;
+                Vector3 forceVector = forceMagnitude * direction;
+
+                rb.AddForce(forceVector);
+            }
+        }
+    }
+}
