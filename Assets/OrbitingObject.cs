dc008c0f1872296b84c7505c5aa79c8d2f12c1dd
Tetratoric <114266944+tetratoric@users.noreply.github.com>
2024-09-28 14:44:35 -0500

    Basic orbital implementation

---
+++ Assets/OrbitingObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(NewtonianObject))]
+public class OrbitingObject : MonoBehaviour
+{
+    public GameObject primaryBody;
+    public float rotationDamp = 0.3f;
+    
+    private void FixedUpdate() {
+        if (primaryBody != null) {
+            Rotate();
+        }
+    }
+
+    void Rotate() {
+        Quaternion target = Quaternion.Euler(Vector3.zero);
+        Vector3 targetDirection = (transform.position - primaryBody.transform.position).normalized;
+        target = Quaternion.FromToRotation(transform.up, targetDirection) * transform.rotation;
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, target, rotationDamp/10);
+    }
+}
